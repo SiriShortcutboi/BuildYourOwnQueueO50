e16bccc9a635611bf9268eacc5dc570afe2568e0
holden.anderson <holden.anderson@students.snow.edu>
2026-04-07 18:39:29 -0600

    need to add to enque and deque

--- BuildYourOwnQueueO50/Program.cs
+++ BuildYourOwnQueueO50/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Runtime.InteropServices.Marshalling;
+
+public class Program
+{
+	public static void Main()
+	{
+		SimpleQueue queue = new SimpleQueue();
+
+        queue.Enqueue("Knight");
+        queue.Enqueue("Mage");
+        queue.Enqueue("Archer");
+
+        Console.WriteLine(queue.Dequeue());
+        Console.WriteLine(queue.Dequeue());
+        Console.WriteLine(queue.Dequeue());
+	}
+}
+
+public class SimpleQueue
+{
+    //Array<string> array = new Array<string> {"1", "2","3","4","5","6","7", "8", "9", "10",};
+    private string[] items = new string[10]; //declare 10 slots automatically
+    private int count = 0;
+
+    Enqueue(string item)
+    {
+        items.Add(inputItem);
+        count++;
+    }
+
+     Dequeue(string item)
+    {
+        items.subtract;//not real needs to change
+        count++;
+
+    }
+}
+
